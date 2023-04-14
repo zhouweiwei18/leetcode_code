d7a306c7d056f74d806ea099ab8d808e28642a66
zhouweiwei18 <765089714@qq.com>
2023-04-14 10:36:38 +0800

    git commit

--- src/com/nuaa/main/sort/QuickSort.java
+++ src/com/nuaa/main/sort/QuickSort.java
@@ -2,27 +2,28 @@ package com.nuaa.main.sort;
 
 public class QuickSort {
 
-    public static void quick_sort(int[] arr, int l, int r) {
-        if (l < r) {
-            int i = l, j = r, x = arr[l];
+    public static void main(String[] args) {
+        int[] arr = new int[]{2, 3, 4, 11, 2, 111, 342, 534, 21, 23, 32, 42, 6};
+        quick_sort(arr, 0, arr.length - 1);
+        // Arrays.sort(arr);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    private static void quick_sort(int[] arr, int left, int right) {
+        if (left < right) {
+            int i = left, j = right;
+            int flag = arr[left];
             while (i < j) {
-                while (i < j && arr[j] >= x) j--;
+                while (i < j && arr[j] >= flag) j--;
                 if (i < j) arr[i++] = arr[j];
-                while (i < j && arr[i] < x) i++;
+                while (i < j && arr[i] < flag) i++;
                 if (i < j) arr[j--] = arr[i];
             }
-            arr[i] = x;
-            quick_sort(arr, l, i - 1);
-            quick_sort(arr, i + 1, r);
+            arr[i] = flag;
+            quick_sort(arr, left, i - 1);
+            quick_sort(arr, i + 1, right);
         }
     }
-
-    public static void main(String[] args) {
-        int[] arrays = new int[]{2, 3, 4, 11, 2, 111, 342, 534, 21, 23, 32, 42, 6};
-        quick_sort(arrays, 0, arrays.length - 1);
-        for (int i = 0; i < arrays.length; i++) {
-            System.out.print(arrays[i] + " ");
-        }
-    }
-
 }
